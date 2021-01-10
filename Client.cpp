438c3cb39ad33caa6d681e728b5cdd6d43916e62
ShakedBu <buch.shaked@gmail.com>
2021-01-10 14:12:09 +0200

    add menu

--- Client.cpp
+++ Client.cpp
@@ -1,27 +1,102 @@
 #include <cstdlib>
 #include <cstring>
+#include <string>
 #include <iostream>
 #include <boost/asio.hpp>
+#include <fstream>
 
+using namespace std;
 using boost::asio::ip::tcp;
 
 enum { max_length = 1024 };
+enum Action {
+    Exit = 0,
+    Register = 1,
+    ClientList = 2,
+    PublicKey = 3,
+    WaitingMessages = 4,
+    SendTextMessage = 5,
+    RequestKey = 51,
+    SendKey = 52
+};
 
 int main(int argc, char* argv[])
 {
     try
     {
-        if (argc != 3)
+        // Default host and port values
+        const char* host = "localhost";
+        const char* port = "1234";
+
+        // Get server's host and port from ths file
+        string serverInfo;
+        ifstream myfile("server.info");
+        if (myfile.is_open())
         {
-            std::cerr << "Usage: blocking_tcp_echo_client <host> <port>\n";
-            return 1;
+            if (myfile.good())
+            {
+                // Get host
+                getline(myfile, serverInfo, ':');
+                host = serverInfo.c_str();
+                cout << serverInfo << endl;
+
+                if (myfile.good())
+                {
+                    // Get port
+                    getline(myfile, serverInfo, ':');
+                    port = serverInfo.c_str();
+                    cout << serverInfo << endl;
+                }
+            }
+
+            myfile.close();
         }
+
 
         boost::asio::io_context io_context;
 
         tcp::socket s(io_context);
         tcp::resolver resolver(io_context);
-        boost::asio::connect(s, resolver.resolve(argv[1], argv[2]));
+        boost::asio::connect(s, resolver.resolve(host, port));
+
+        while (true) {
+            std::cout << "MessageU client at your service! " << std::endl <<
+                "1) Register" << std::endl <<
+                "2) Request for client list" << std::endl <<
+                "3) Request for public key" << std::endl <<
+                "4) Request for waiting messages" << std::endl <<
+                "5) Send a text message" << std::endl <<
+                "51) Send a request for symmetric key" << std::endl <<
+                "52) Send your semmetric key" << std::endl <<
+                "0) Exit client" << std::endl;
+
+            int action;
+            std::cin >> action;
+
+            switch (action)
+            {
+                case Exit:
+                    return 0;
+                    break;
+                case Register:
+                    break;
+                case ClientList:
+                    break;
+                case PublicKey:
+                    break;
+                case WaitingMessages:
+                    break;
+                case SendTextMessage:
+                    break;
+                case RequestKey:
+                    break;
+                case SendKey:
+                    break;
+                default:
+                    std::cout << "Please enter a valid number." << std::endl;
+                    break;
+            }
+        }
 
         std::cout << "Enter message: ";
         char request[max_length];
@@ -38,7 +113,7 @@ int main(int argc, char* argv[])
     }
     catch (std::exception& e)
     {
-        std::cerr << "Exception: " << e.what() << "\n";
+        std::cerr << "server responded with an error" << std::endl;
     }
 
     return 0;
